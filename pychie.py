f5832af64e382548404883883d550996801c50c7
Clurtyx <58487962+Clurtyx@users.noreply.github.com>
2022-04-29 03:46:59 +0300

    Add files via upload

---
+++ pychie.py
@@ -0,0 +1,9 @@
+import os
+print("Hello! Welcome to the 'Pychie' installer!\n Let's get you set up and ready to go!")
+time.sleep(3)
+intro = input("Press 'Enter' to Begin!")
+if intro == "":
+     print("Let's go!")
+else:
+    print("ouch! let's try that again")
+    break
