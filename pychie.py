d16ed93c8983d791eefa344ce21f93f0f02cbb2e
clurtyx <clurtyxowo@gmail.com>
2022-04-29 08:21:01 +0300

    test

--- pychie.py
+++ pychie.py
@@ -1,9 +1,12 @@
 import os
+import time
 print("Hello! Welcome to the 'Pychie' installer!\n Let's get you set up and ready to go!")
 time.sleep(3)
 intro = input("Press 'Enter' to Begin!")
 if intro == "":
      print("Let's go!")
 else:
-    print("ouch! let's try that again")
-    break
+    print("Close enough! ")
+print("First, let's get your partitions in order.")
+time.sleep(1)
+os.system("lsblk && fdisk -l")
