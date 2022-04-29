280ec78a78dbaf1fb329904a80f2f08dcbfcba61
vexy <clurtyxowo@gmail.com>
2022-04-29 23:34:36 +0300

    update

--- pychie.py
+++ pychie.py
@@ -3,12 +3,18 @@ import time
 print("Hello! Welcome to the 'Pychie' installer!\n Let's get you set up and ready to go!")
 time.sleep(3)
 intro = input("Press 'Enter' to Begin!")
+if intro == "mqn":
+    print("You have found the easter egg! Mqn nutove moment")
 if intro == "":
      print("Let's go!")
-else:
-    print("Close enough! ")
+
 print("First, let's get your partitions in order.")
 time.sleep(1)
-os.system("lsblk && fdisk -l")
-disk = input("Which disk would you like to install Arch to?: ")
-os.system("cfdisk " disk)
+def intro():
+    os.system("lsblk && fdisk -l")
+    disk = input("Which disk would you like to install Arch to?: ")
+    installDrive = input("Is this your drive? (Y,n): ", disk)
+    if installDrive == "n":
+        return False
+while intro():
+    pass
